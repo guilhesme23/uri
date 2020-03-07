981251ca1f5c2be2e75cd0663017b181ff6dd44a
Guilherme <marquesguilherme3@gmail.com>
2020-03-06 22:55:02 -0300

    1222

--- haskell/Strings/1222.hs
+++ haskell/Strings/1222.hs
@@ -1,13 +1,11 @@
 import Data.List
-import Data.Ord (comparing)
 import System.IO (isEOF)
 
 ss ll cc (pages, lls, ccs) x
     | at <= cc = (pages, lls, at)
-    | ls = (pages, lls+1, x)
-    | otherwise = (pages+1, 1, 0)
+    | at > cc && lls < ll = (pages, lls + 1, x)
+    | at > cc && lls >= ll = (pages+1, 1, x)
     where at = if ccs > 0 then x + ccs + 1 else x
-          ls = at > cc && lls < ll
 
 main = do
     exit <- isEOF
@@ -15,12 +13,6 @@ main = do
         return ()
     else do
         (n:ll:cc:_) <- (map read.words) `fmap` getLine :: IO [Int]
-        a <- fmap (intersperse 1.map length.words) getLine
-        let res = scanl (ss ll cc) (1, 1, 0) a
-            cccs = if even (length a) then (sum a - 1 `div` cc) else sum a `div` cc
-            pps = cccs `div` ll
-            us = cccs `mod` ll
-        -- print res
-        print $ sum a
-        print (pps, us, sum a `mod` cc)
+        a <- fmap (map length.words) getLine
+        print $ (\(a, b, c) -> a) $ foldl (ss ll cc) (1, 1, 0) a
         main
